9bf21e37f2b8dda7930c432758dbb674542be0d6
Justin Sooknanan <114186684+JuiceT1n@users.noreply.github.com>
2026-03-14 10:03:13 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_fa_1bit_behavioural user_project (
+  tt_um_fa_1bit_behavioural user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
